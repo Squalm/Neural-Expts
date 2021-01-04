511ecad70ada33877c3276adcf4bdee178ee8092
Squalm <43246103+Squalm@users.noreply.github.com>
2021-01-04 20:17:27 +0000

    move

---
+++ Go/Main.jl
@@ -0,0 +1,6 @@
+## Outline
+# First, we want an algorithm that learns to replicate others's moves.
+# Then, we want an algo that plays whole games to learn
+# That is likely combined with a search algo.
+
+## Step The First: Copy
---
+++ Go/importsgf.jl
@@ -0,0 +1,33 @@
+"""
+    import_game(path)
+
+`path`: relative path to the game to import.
+"""
+function import_game(path::String)
+
+    states = []
+    open(path) do f
+
+        contents = read(f, String)
+        nodes = findall(";", contents)
+        for i in range(1, length = length(nodes))
+
+            node = contents[nodes[i]:nodes[i + 1]]
+
+            # First, find who the winner was.
+            result = node[findfirst("RE[*]", node)]
+            println(result)
+
+            if contents[nodes[i] + 1] == "B" || contents[nodes[i] + 1] == "W"
+
+            end # if
+
+        end # for
+
+    end # do
+
+    return states
+
+end # function
+
+import_game("Go\\Training\\AI\\AlphaGo Zero\\Extended Data Figure 1 - AlphaGo Zero 20 block vs AlphaGo Lee\\Game_001.sgf")
