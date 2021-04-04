a39fe1e12b1a58f233d357e49627acbf01e418ad
Squalm <43246103+Squalm@users.noreply.github.com>
2021-04-04 22:11:21 +0100

    moving this stuff

--- Go/Main.jl
+++
@@ -1,6 +0,0 @@
-## Outline
-# First, we want an algorithm that learns to replicate others's moves.
-# Then, we want an algo that plays whole games to learn
-# That is likely combined with a search algo.
-
-## Step The First: Copy
--- Go/Requirements.jl
+++
@@ -1,8 +0,0 @@
-# Run this first if you don't have the modules installed.
-
-# Requirements:
-# CUDA
-# Flux
-
-import Pkg; Pkg.add("CUDA")
-import Pkg; Pkg.add("Flux")
